a4d754da9648245344b503ae344a60621bc230c7
michela iacobucci <michelaiacobucci@MacBook-Air.local>
2020-05-31 18:36:45 -0700

    progress531

--- js/app.js
+++ js/app.js
@@ -11,9 +11,21 @@ document.addEventListener('DOMContentLoaded', function(){
     let downRight = document.getElementById('08');
     let resetBtn = document.getElementById('reset'); 
 
+ /* Phillip wanted me to use a for loop but when googling it, articles said it's best not to since it renders the previous index   */  
+    let cells = document.getElementsByClassName('.inner');
+    console.log(cells);
+
+    for (leti= 0; i < cells.length; i++) {
+        cells.addEventListener ('click', function(){
+            console.log(i)
+        })
+    }
+
 /*----Game Logic Variables ----*/
     let t3 = document.getElementById('tictactoe');
-    let gameOver = true; 
+
+    // let gameOver = true; 
+
     let winningCombo = [
         ["00", "01" , "02"],
         ["03", "04", "05"],
@@ -24,8 +36,12 @@ document.addEventListener('DOMContentLoaded', function(){
         ["01" , "04" , "07"],
         ["02" , "05" , "08"]
     ]; 
+
+    let player1 = "X"
+    let player2 = "O"
   
 /*--- player selection ----*/ 
+// the function will see if there is a remainder of 0 between two players, if so it'll prompt the other player if not, it'll stop 
 
     function getTurn () {
         if (turn % 2 !== 0) {
@@ -42,19 +58,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
     console.log (updateScore); 
 
-    let player1 = {
-        points: 0,
-        currentChoice: null
-    };
-
-    let player2 = {
-        points: 0,
-        currentChoice: null
-    };
-
-    let turn = 0; 
-
-/*--- Event Listeners ----*/ 
+/*--- Event Listeners ----*/ /* DRY
 
     resetBtn.addEventListener('click', reset);
     t3.addEventListener('click', boxClick); 
@@ -66,10 +70,76 @@ document.addEventListener('DOMContentLoaded', function(){
     midRight.addEventListener('click', boxClick);
     downLeft.addEventListener('click', boxClick);
     downMid.addEventListener('click', boxClick);
-    downRight.addEventListener('click', boxClick);
+    downRight.addEventListener('click', boxClick); 
+
+    */
+    
+
+/* --- Adding X's and O's --- */
+
+function play (e) {
+    console.log("The turn is " + e.target.id);
+    let playerTurns = turn ();
+    if (e.target.id === '00') {
+        if (playerTurns) {
+            upLeft.textContent = 'X';
+        } else {
+            upLeft.textContent = 'O'; 
+        }
+      } if (e.target.id === '01') {
+          if (playerTurns) {
+              upMid.textContent = 'X';
+          } else {
+              upMid.textContent = 'O';
+          }
+      } if (e.target.id === '02') {
+          if (playerTurns) {
+            upRight.textContent = 'X';
+        } else {
+            upRight.textContent = 'O';
+        }
+    } if (e.target.id === '03') {
+        if (playerTurns) {
+            midLeft.textContent = 'X';
+        } else {
+            midLeft.textContent = 'O';
+        }
+    } if (e.target.id === '04') {
+        if (playerTurns) {
+            upMid.textContent = 'X';
+        } else {
+            upMid.textContent = 'O';
+        }
+    } if (e.target.id === '05') {
+        if (playerTurns) {
+            upRight.textContent = 'X';
+        } else {
+            upRight.textContent = 'O';
+        }
+    } if (e.target.id === '06') {
+        if (playerTurns) {
+            downLeft.textContent = 'X';
+        } else {
+            downLeft.textContent = 'O';
+        }
+    } if (e.target.id === '07') {
+        if (playerTurns) {
+            downMid.textContent = 'X';
+        } else {
+            downMid.textContent = 'O';
+        }
+    } if (e.target.id === '08') {
+        if (playerTurns) {
+            downRight.textContent = 'X';
+        } else {
+            downRight.textContent = 'O';
+        }
+    }
+    }
+}
 
 
-    // Box Chosen
+    // Box Chosen - learned through TA with Dope Lemon 
     function boxClick(e) {
         console.log("You chose " + e.target.id)
         let box = e.target.id;
@@ -78,16 +148,41 @@ document.addEventListener('DOMContentLoaded', function(){
             if (9 > winningCombo[i]) continue;
             combo = false;
             break;
-        }
-        if (combo) alert("Cat's tongue"); 
+        } 
+        if (combo) return("Cat's tongue"); 
         }
 
-        // If the game is not over and the opponent hasn't won
-        if (if (turn % 2 === 0) { //player 1, change element to x
+        
+
+       /* if (if (turn % 2 === 0) { //player 1, change element to x
 } else {
-  // player 2, change element to o
+// player 2, change element to */ 
 
-        if (player1.boxClick === ) */
+// Checking for a win - learned through TA hours with Phillip
 
-    } 
-})
+        function winCheck() {
+            for (let i = 0; i < winningCombos.length; i++) {
+                let playerCountX = 0;
+                let playerCountO = 0;
+                for (let j = 0; j < winningCombos[i].length; j++) {
+                    console.log(playerXChooses);
+                    console.log(winningCombos[i][j]);
+                    if (playerXChooses.includes(winningCombos[i][j])) {
+                        console.log("player one check");
+                        playerCountX++;
+                        console.log("score = " + playerCountX);
+                    } if (playerOChooses.includes(wininngCombos[i][j])) {
+                        console.log("player two check");
+                        playerCountO++;
+                        console.log(playerCountO);
+                    } if (playerXChooses.includes(winningCombos[i][j] && playerCountX === 3)) {
+                        console.log("player one wins!");
+                    } if (playerOChooses.includes(winningCombos[i][j]) && playerCountO === 3) {
+                        console.log("player two wins!");
+                    }
+                }
+            }
+        }
+    }
+
+,)
